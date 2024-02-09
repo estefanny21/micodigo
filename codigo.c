803f2666e79acff2a3661adc734cb66306d5e051
estefanny21 <159509939+estefanny21@users.noreply.github.com>
2024-02-08 22:49:20 -0600

    Update and rename codigo to codigo.c

---
+++ codigo.c
@@ -0,0 +1,15 @@
+// Simple C program to display "Hello World"
+
+// Header file for input output functions
+#include <stdio.h>
+
+// main function -
+// where the execution of program begins
+int main()
+{
+
+	// prints hello world
+	printf("LA bienvenida de los datos");
+
+	return 0;
+}
